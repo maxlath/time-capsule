e0a7fd78cc1e6e0ddf9543d6e4119b5c84bbf386
maxlath <k@maxlath.eu>
2016-07-30 16:56:09 +0200

    + _.log

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -1,17 +1,12 @@
-module.exports = {
-  Log: function (label) {
-    return function (obj) {
-      console.log(label, obj)
-      return obj
-    }
+const utils = module.exports = {
+  log: (obj, label) => {
+    console.log(label, obj)
+    return obj
   },
-  Error: function (label) {
-    return function (err) {
-      console.error(label, err)
-    }
-  },
-  ErrorRethrow: function (label) {
-    return function (err) {
+  Log: (label) => (obj) => utils.log(label, obj),
+  Error: (label) => (err) => console.error(label, err),
+  ErrorRethrow: (label) => {
+    return (err) => {
       console.error(label, err)
       throw err
     }
