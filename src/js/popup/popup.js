0144636d86b29d9405e096b6a7fd63b49ffdbcc0
maxlath <k@maxlath.eu>
2017-04-02 23:47:52 +0200

    popup: attempt to autofocus when opening

--- src/js/popup/popup.js
+++ src/js/popup/popup.js
@@ -3,7 +3,12 @@ const tabs = require('../lib/tabs')
 const buildPopup = require('./build_popup')
 const listenForKeyboardEvents = require('./listen_for_keyboard_events')
 
+const autofocus = () => document.querySelector('.selected').focus()
+
 tabs.getCurrentUrlBookmarkData()
 .then(buildPopup)
 .then(listenForKeyboardEvents)
+// Popup doesn't autofocus
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1338909
+.then(() => setTimeout(autofocus, 500))
 .catch(_.Error('popup'))
