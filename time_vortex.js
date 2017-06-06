18a997d558a33c5f2a54736d9a33a1f9e5b9852a
maxlath <k@maxlath.eu>
2017-06-06 11:51:13 +0200

    added time_vortex.js

---
+++ time_vortex.js
@@ -0,0 +1,18 @@
+const timeVortex = require('time-vortex')
+const { EventEmitter } = require('events')
+const radio = new EventEmitter()
+const timeLinearisationRatio = (1 + Math.pow(5, 0.5)) / 2
+const easeInOutQuad = (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+
+timeVortex
+.listen(7777)
+.on('init', res => {
+  const { timeAndRelativeDimensionInSpace, wibblyWobblyRatio } = res
+  radio.emit('time:vortex:init', wibblyWobblyRatio / timeLinearisationRatio, timeAndRelativeDimensionInSpace)
+})
+.on('data', capsule => {
+  capsule.timeLandingSpeedEasingFn = easeInOutQuad
+  radio.emit('time:capsule', capsule)
+})
+
+module.exports = radio
