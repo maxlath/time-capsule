784c5307bd1d063a7a02ad8eac5049bd077846a9
maxlath <k@maxlath.eu>
2021-08-24 23:29:33 +0200

    recover browser action popup shortkey

--- src/background/background.js
+++ src/background/background.js
@@ -1,12 +1,8 @@
-// import { openPopup, closePopup } from '../lib/browser_action'
 import './keep_icon_in_sync'
 import './open_bookmarks_periodically'
 
-// browser.commands.onCommand.addListener(command => {
-//   if (command === 'open-periodicity-menu') {
-//     openPopup()
-//     // If open popup throws an error, it's probably that a popup is already opened
-//     // in which case, the command is interpreted as 'close the popup'
-//     .catch(closePopup)
-//   }
-// })
+browser.commands.onCommand.addListener(command => {
+  if (command === 'open-periodicity-menu') {
+    browser.browserAction.openPopup()
+  }
+})
