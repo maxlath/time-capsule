4e54bde3dc8c94419f89fa4a67b5eb37c49e8200
maxlath <k@maxlath.eu>
2022-11-28 17:12:48 +0100

    settings: Capsules: extract table to CapsuleTable

---
+++ src/settings/CapsuleTable.svelte
@@ -0,0 +1,122 @@
+<script>
+  import { getBookmarks } from '../lib/bookmarks'
+  import { sortFunctions } from '../lib/sort_capsules'
+  import Spinner from '../popup/Spinner.svelte'
+  import CapsuleRow from './CapsuleRow.svelte'
+  import { i18n } from '../lib/i18n'
+  import { onChange } from '../lib/svelte'
+
+  export let editedBookmark
+
+  let bookmarks, sortedBookmark, bookmarksPage
+  let sortField = 'nextVisit'
+  let reverseSort = false
+
+  const waitForBookmarks = getBookmarks()
+    .then(b => bookmarks = b)
+
+  $: {
+    if (bookmarks) {
+      sortedBookmark = bookmarks.sort(sortFunctions[sortField])
+    }
+  }
+
+  $: {
+    if (bookmarks) {
+      bookmarksPage = reverseSort ? sortedBookmark.slice(0).reverse() : sortedBookmark
+    }
+  }
+
+  const columns = [
+    { label: 'Title', field: 'title' },
+    { label: 'Frequency', field: 'frequency' },
+    { label: 'Next_Visit', field: 'nextVisit' },
+    { label: 'Date_Created', field: 'dateAdded' },
+    { label: 'Actions' },
+  ]
+
+  function resort (e) {
+    if (e.target.name === sortField) {
+      reverseSort = !reverseSort
+    } else {
+      sortField = e.target.name
+      reverseSort = false
+    }
+  }
+
+  function updateBookmark () {
+    bookmarks = bookmarks
+  }
+
+  $: onChange(editedBookmark, updateBookmark)
+</script>
+
+{#await waitForBookmarks}
+  <Spinner />
+{:then}
+  <table>
+    <thead>
+      <tr>
+        {#each columns as column}
+          <th>
+            <button
+              name={column.field}
+              class:active={sortField === column.field}
+              class:reversed={sortField === column.field && reverseSort}
+              disabled={column.field == null}
+              on:click={resort}
+            >{i18n(column.label)}</button>
+          </th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each bookmarksPage as bookmark (bookmark.id)}
+        <CapsuleRow {bookmark} on:edit={() => editedBookmark = bookmark} />
+      {/each}
+    </tbody>
+  </table>
+{/await}
+
+<style>
+  table{
+    border-collapse: collapse;
+    width: 100%;
+  }
+  th{
+    font-weight: normal;
+    padding: 0;
+  }
+  th{
+    text-align: left;
+  }
+  th button{
+    padding: 0.5em 1.5em 0.5em 0.5em;
+    width: 100%;
+    font-size: 1rem;
+    position: relative;
+    background-color: #eee;
+    transition: background-color 0.3s ease;
+  }
+  button:disabled{
+    color: inherit;
+  }
+  th button.active, th button:not(:disabled):hover{
+    background-color: #ddd;
+  }
+  th button.active:after{
+    content: '▲';
+    color: #777;
+    position: absolute;
+    top: 10%;
+    right: 0.5em;
+  }
+  th button.reversed:after{
+    top: auto;
+    transform: rotate(180deg);
+    bottom: 10%;
+  }
+  tr:nth-child(even) {
+    background: #eee;
+  }
+</style>
--- src/settings/Capsules.svelte
+++ src/settings/Capsules.svelte
@@ -1,54 +1,8 @@
 <script>
-  import { getBookmarks } from '../lib/bookmarks'
-  import { sortFunctions } from '../lib/sort_capsules'
-  import Spinner from '../popup/Spinner.svelte'
-  import CapsuleRow from './CapsuleRow.svelte'
   import CapsuleEditor from '../popup/CapsuleEditor.svelte'
-  import { i18n } from '../lib/i18n'
-  import { onChange } from '../lib/svelte'
+  import CapsuleTable from './CapsuleTable.svelte'
 
-  let bookmarks, sortedBookmark, bookmarksPage
-  let sortField = 'nextVisit'
-  let reverseSort = false
   let editedBookmark
-
-  const waitForBookmarks = getBookmarks()
-    .then(b => bookmarks = b)
-
-  $: {
-    if (bookmarks) {
-      sortedBookmark = bookmarks.sort(sortFunctions[sortField])
-    }
-  }
-
-  $: {
-    if (bookmarks) {
-      bookmarksPage = reverseSort ? sortedBookmark.slice(0).reverse() : sortedBookmark
-    }
-  }
-
-  const columns = [
-    { label: 'Title', field: 'title' },
-    { label: 'Frequency', field: 'frequency' },
-    { label: 'Next_Visit', field: 'nextVisit' },
-    { label: 'Date_Created', field: 'dateAdded' },
-    { label: 'Actions' },
-  ]
-
-  function resort (e) {
-    if (e.target.name === sortField) {
-      reverseSort = !reverseSort
-    } else {
-      sortField = e.target.name
-      reverseSort = false
-    }
-  }
-
-  function updateBookmark () {
-    bookmarks = bookmarks
-  }
-
-  $: onChange(editedBookmark, updateBookmark)
 </script>
 
 {#if editedBookmark}
@@ -68,75 +22,10 @@
     </div>
   </div>
 {:else}
-  {#await waitForBookmarks}
-    <Spinner />
-  {:then}
-    <table>
-      <thead>
-        <tr>
-          {#each columns as column}
-            <th>
-              <button
-                name={column.field}
-                class:active={sortField === column.field}
-                class:reversed={sortField === column.field && reverseSort}
-                disabled={column.field == null}
-                on:click={resort}
-              >{i18n(column.label)}</button>
-            </th>
-          {/each}
-        </tr>
-      </thead>
-      <tbody>
-        {#each bookmarksPage as bookmark (bookmark.id)}
-          <CapsuleRow {bookmark} on:edit={() => editedBookmark = bookmark} />
-        {/each}
-      </tbody>
-    </table>
-  {/await}
+  <CapsuleTable bind:editedBookmark />
 {/if}
 
 <style>
-  table{
-    border-collapse: collapse;
-    width: 100%;
-  }
-  th{
-    font-weight: normal;
-    padding: 0;
-  }
-  th{
-    text-align: left;
-  }
-  th button{
-    padding: 0.5em 1.5em 0.5em 0.5em;
-    width: 100%;
-    font-size: 1rem;
-    position: relative;
-    background-color: #eee;
-    transition: background-color 0.3s ease;
-  }
-  button:disabled{
-    color: inherit;
-  }
-  th button.active, th button:not(:disabled):hover{
-    background-color: #ddd;
-  }
-  th button.active:after{
-    content: '▲';
-    color: #777;
-    position: absolute;
-    top: 10%;
-    right: 0.5em;
-  }
-  th button.reversed:after{
-    top: auto;
-    transform: rotate(180deg);
-    bottom: 10%;
-  }
-  tr:nth-child(even) {
-    background: #eee;
-  }
   .editor-wrapper{
     display: flex;
     flex-direction: row;
