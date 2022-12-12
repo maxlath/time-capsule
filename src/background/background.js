6c5047cd517d4d4078a9fbf79aabe5ec621f8ec4
maxlath <k@maxlath.eu>
2022-12-12 16:33:38 +0100

    manifest: add shortcut to open settings

--- src/background/background.js
+++ src/background/background.js
@@ -4,5 +4,7 @@ import './open_bookmarks_periodically'
 browser.commands.onCommand.addListener(command => {
   if (command === 'open-periodicity-menu') {
     browser.browserAction.openPopup()
+  } else if (command === 'open-periodicity-settings') {
+    browser.tabs.create({ url: '/settings/settings.html' })
   }
 })
