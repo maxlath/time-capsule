b986a8c9a5f78ed105f1a807bc49c9b2351be70b
maxlath <k@maxlath.eu>
2022-12-14 01:09:24 +0100

    popup: close when the tab changes

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,3 +4,8 @@ new Popup({
   target: document.querySelector('main'),
   props: {}
 })
+
+// The popup is coupled to the URL when it was opened
+// so if that changes, it should be reopened
+browser.tabs.onActivated.addListener(() => window.close())
+browser.tabs.onUpdated.addListener(() => window.close())
